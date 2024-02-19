48ae48a58ea99ce7e5c5294e266f9c355a325da9
Zahraa A. AlAbdullah <109365947+Possibly-Necessary@users.noreply.github.com>
2024-02-19 10:04:39 +0300

    Update asymmetric-crypto.go

--- MPC-Golang/asymmetric-crypto.go
+++ MPC-Golang/asymmetric-crypto.go
@@ -29,14 +29,14 @@ func txtBookRSA(bitSize int) (*big.Int, *big.Int, *big.Int) { // Returns e, d, N
 }
 
 // Encryption/Decryption using the textbook RSA we defined earlier
-// Function that encryptes messages using the public key (e, N)
+// Function that encrypts messages using the public key (e, N)
 func Encrypt(message []byte, e, N *big.Int) *big.Int {
 	m := new(big.Int).SetBytes(message)
 	c := new(big.Int).Exp(m, e, N) // c = m^e mod N
 	return c
 }
 
-// Function that decryptes the messages 
+// Function that decrypts the messages 
 func Decrypt(c, d, N *big.Int) string {
 	m := new(big.Int).Exp(c, d, N) // m = c^d mod N
 
